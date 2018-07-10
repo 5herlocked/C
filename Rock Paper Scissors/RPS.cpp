e67540b8f856d4f657beb417285e525c11a4c935
Shadow <cam.v737@gmail.com>
2018-07-09 22:42:40 -0400

    2

--- Rock Paper Scissors/RPS.cpp
+++ Rock Paper Scissors/RPS.cpp
@@ -3,132 +3,71 @@
 
 using namespace std;
 
-class RPS 
+class RPS
 {
-    private:
-        string board[3][3];
-        bool move;
-        string player1;
-        string player2;
-        int numberOfMoves;
-        bool rowCheck( int rowNum );
-
     public:
         RPS();
-        RPS( string player1, string player2 );
-        char nextMove( int location );
-        void createBoard();
-        string getPlayer1();
-        string getPlayer2();
-        char endCriteria();
-        bool getMove();
-};
+        RPS(string player1);
+        char compMove();
+        char nextProMove(char userInput);
+        string getPlayer1Name();
+        char checkVictory(char player1Move, char compMove);
+
+    private:
+        string player1Name;
+        char[3] choices;
+        char compMove;
+        char getNextMove(int options, char userInput); //option 1 = rand, option 2 = choose
+}
 
 RPS::RPS()
 {
-    move = true;
+    player1Name = "Jane Doe";
 
-    board[0] = { "1" , "2" , "3" };
-    board[1] = { "4" , "5" , "6" };
-    board[2] = { "7" , "8" , "9" };
-
-    player1 = "John Doe";
-    player2 = "Jane Doe";
-    
-    createBoard();
+    choices[0] = 'R';
+    choices[1] = 'P';
+    choices[2] = 'S';
 }
 
-RPS::RPS ( string player1, string player2 )
+RPS::RPS(string player1)
 {
-    move = true;
-    
-    RPS::player1 = player1;
-    RPS::player2 = player2;
+    player1Name = player1;
 
-    int location = 1;
+    choices[0] = 'R';
+    choices[1] = 'P';
+    choices[2] = 'S';
+}
 
-    for (auto& rows : board)
+RPS::compMove()
+{
+    compMove = getNextMove(1);
+
+    return compMove;
+}
+
+RPS::getNextMove(char userInput)
+{
+    compMove = getNextMove(2);
+
+    return compMove;
+}
+
+RPS::getPlayer1Name(){ return player1Name; }
+
+RPS::checkVictory(char player1Move, char compMove)
+{
+    switch (player1Move)
     {
-        for (auto& c : rows)
-        {
-            c = location++;
-        }
+        player1Move == compMove:
+            return 't';
+        
+        player1Move == 'R':
+            return ( compMove == 'P' ) ? 'c' : ( compMove == 'S' ) ? 'p' : 't';
+        
+        player1Move == 'S':
+            return ( compMove == 'P' ) ? 'p' : ( compMove == 'R' ) ? 'c' : 't';
+
+        player1Move == 'P':
+            return ( compMove == 'S' ) ? 'c' : ( compMove == 'R' ) ? 'p' : 't';
     }
-
-    // for (int r = 0; r < 3; ++r)
-    // {
-    //     for (int c = 0; c < 3; ++c)
-    //     {
-    //         ++location;
-    //         board[r][c] = location;
-    //     }
-    // }
-
-    createBoard();
-}
-
-char RPS::nextMove( int location )
-{
-    board[ location/3 ][ location%3 ] = ( move ) ? 'X' : 'O';
-    ++numberOfMoves;
-    move = !move;
-    createBoard();
-}
-
-void RPS::createBoard ()
-{
-    cout << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << '\n' ;
-    cout << "---------" << '\n';
-    cout << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << '\n' ;
-    cout << "---------" << '\n';
-    cout << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << '\n' ;
-}
-
-string RPS::getPlayer1 ()
-{
-    return player1;
-}
-
-string RPS::getPlayer2 ()
-{
-    return player2;
-}
-
-bool RPS::rowCheck ( int rowNum )
-{
-    for (int i = 0; i < 3; i++)
-    {
-        if (board[rowNum][i].compare(board[rowNum][0]) != 0)
-            return false;
-    }
-
-    return true;
-}
-
-char RPS::endCriteria ()
-{
-    if (board[0][0].compare(board[0][1]) == 0  && board[0][1].compare(board[0][2]) == 0 && (board[0][2].compare("X") == 0 || board[0][2].compare("O") == 0))
-        return board[0][0].compare("X") == 0 ? '1' : '2';
-
-    if (board[1][0].compare(board[1][1]) == 0 && board[1][1].compare(board[1][2]) == 0 && (board[1][2].compare("X") == 0 || board[1][2].compare("O") == 0))
-        return board[1][0].compare("X") == 0 ? '1' : '2';
-
-    if (board[2][0].compare(board[2][1]) == 0 && board[2][1].compare(board[2][2]) == 0 && (board[2][2].compare("X") == 0 || board[2][2].compare("O") == 0))
-        return board[2][0].compare("X") == 0 ? '1' : '2';
-
-    if (board[0][0].compare(board[1][0]) == 0 && board[1][0].compare(board[2][0]) == 0 && (board[2][0].compare("X") == 0 || board[2][0].compare("O") == 0))
-        return board[0][0].compare("X") == 0 ? '1' : '2';
-
-    if (board[0][1].compare(board[1][1]) == 0 && board[1][1].compare(board[2][1]) == 0 && (board[2][1].compare("X") == 0 || board[2][1].compare("O") == 0))
-        return board[0][1].compare("X") == 0 ? '1' : '2';
-
-    if (board[0][2].compare(board[1][2]) == 0 && board[1][2].compare(board[2][2]) == 0 && (board[2][2].compare("X") == 0 || board[2][2].compare("O") == 0))
-        return board[0][2].compare("X") == 0 ? '1' : '2';
-
-    return 't';
-}
-
-bool RPS:: getMove ()
-{
-    return move;
 }
--- Rock Paper Scissors/playRPS.cpp
+++ Rock Paper Scissors/playRPS.cpp
@@ -3,29 +3,5 @@
 
 int main ()
 {
-    RPS game = RPS();
-
-    // cout << "This is 2 Player Rock Paper Scissors Game!" << '\n' ;
-
-    // cout << "Please enter Player 1's Name (gets X): \n";
-    // string player1;
-    // getline(cin, player1);
-
-    // cout << "Please enter Player 2's Name (gets O): \n";
-    // string player2;
-    // getline(cin, player2);
-
-    // cout << "The board: \n";
-    // RPS game = RPS(player1 , player2);
-
-    // int location = 0;
-
-    // while ()
-    // {
-    //     cout << "Please enter the location for your move " << (game.getMove() ? game.getPlayer1() : game.getPlayer2()) << '\n';
-
-    //     cin >> location;
-    // }
-
     return 0;
 }
